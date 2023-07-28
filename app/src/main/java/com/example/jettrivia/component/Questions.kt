15c254c7b983e2d615c3371b06978f9527d503e7
ThorstiFinki <105248638+ThorstiFinki@users.noreply.github.com>
2023-07-28 11:36:35 +0200

    Questions angepasst

--- app/src/main/java/com/example/jettrivia/component/Questions.kt
+++ app/src/main/java/com/example/jettrivia/component/Questions.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
@@ -51,12 +53,27 @@ fun Questions(viewModel: QuestionsViewModel) {
 
     val questions =
         viewModel.data.value.data?.toMutableList()
+
+    val questionIndex = remember {
+        mutableStateOf(0)
+    }
+
+
+
     if (viewModel.data.value.loading == true) {
         CircularProgressIndicator()
         Log.d("Loading", "Questions: ...loading....")
     } else {
+        val question = try{
+           questions?.get(questionIndex.value)
+        }catch (ex: Exception){
+            null
+        }
         if (questions !=null){
-            QuestionDisplay(question = questions.first())
+            QuestionDisplay(question = question!!, questionIndex = questionIndex,
+            viewModel = viewModel){
+                questionIndex.value = questionIndex.value +1
+            }
         }
     }
 }
@@ -83,8 +100,8 @@ fun DrawDottedLine(pathEffect: PathEffect){
     @Composable
     fun QuestionDisplay(
        question: QuestionItem,
-     //  questionIndex: MutableState<Int>,
-     //  viewModel: QuestionsViewModel,
+       questionIndex: MutableState<Int>,
+       viewModel: QuestionsViewModel,
        onNextClicked: (Int) -> Unit ={}
     ){
        val choicesState = remember(question) {
@@ -104,14 +121,13 @@ fun DrawDottedLine(pathEffect: PathEffect){
      val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f,10f),0f)
         Surface(modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(4.dp),
+            .fillMaxHeight(),
             color = AppColors.mDarkPurple){
 
             Column(modifier = Modifier.padding(12.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start) {
-                QuestionTracker()
+                QuestionTracker(counter = questionIndex.value)
                 DrawDottedLine(pathEffect = pathEffect)
 
                 Column {
@@ -160,13 +176,41 @@ fun DrawDottedLine(pathEffect: PathEffect){
                                 }else{
                                     Color.Red.copy(alpha =0.2f)
                                 })) // end RadioButton
-                          
-                          Text(text = answerText)
+
+                          val annotatedString = buildAnnotatedString {
+                              withStyle(style= SpanStyle(fontWeight = FontWeight.Light,
+                              color=
+                                  if (correctAnswerState.value == true
+                                      && index == answerState.value) {
+                                      Color.Green
+                                  }else if(correctAnswerState.value == true &&
+                                          index ==answerState.value) {
+                                      Color.Red
+                                  }else{AppColors.mOffWhite}, fontSize = 17.sp)
+                              )
+                              {
+                             append(answerText)
+                              }
+
                       }
+                          Text(text = annotatedString, modifier = Modifier.padding(6.dp))
                     }
 
-
                 }
+                    Button(onClick = { onNextClicked(questionIndex.value) },
+                        modifier = Modifier
+                            .padding(3.dp)
+                            .align(alignment = Alignment.CenterHorizontally),
+                        shape= RoundedCornerShape(34.dp),
+                        colors = ButtonDefaults.buttonColors(
+                           containerColor =  AppColors.mLightBlue)
+                        ){
+                        Text(text = "Next",
+                        modifier = Modifier.padding(4.dp),
+                        color = AppColors.mOffWhite,
+                        fontSize = 17.sp)
+                    }
+                    }
 
             }
 
